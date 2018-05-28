067f8717c7d4079b301ffbeace5e046dea328f37
mikola-s <suharev.nv@gmail.com>
2018-05-28 13:34:28 +0300

    start

---
+++ module_for_test.php
@@ -0,0 +1,88 @@
+<?php
+
+
+
+
+
+/**
+ * 
+ */
+class User 
+{
+	public name, password, acess;
+
+	public function __construct(argument)
+	{
+		# code...
+	}
+
+	//добавить пользователя 
+	public function add(){
+
+	}
+
+	//поиск пользователя 
+	public function find(){
+
+	}
+
+	//сохранить пользователя
+	public function save(){
+
+	}
+
+	//удалить 
+	public function delete()
+	{
+		# code...
+	}
+}
+
+
+/**
+ * 
+ */
+class News
+{
+	public date, time, content, full;
+
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	//
+	public function add($value='')
+	{
+		# code...
+	}
+
+	//
+	public function delete($value='')
+	{
+		# code...
+	}
+
+	public function edit($value='')
+	{
+		# code...
+	}
+
+	public function save($value='')
+	{
+		# code...
+	}
+}
+
+/**
+ * 
+ */
+class pages
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
